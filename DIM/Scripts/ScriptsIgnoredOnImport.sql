91c4a79997f8d432dd5f9c75619c87c6a0b74696
Arun M <arunm6@deloitte.com>
2021-04-05 10:39:35 +0530

    project dacpac commit

---
+++ DIM/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,41 @@
+﻿
+CREATE TABLE dim.test
+(
+    customer_id bigint,
+    cc_unique_account_id bigint,
+    outstanding_balance double precision,
+    issue_date timestamp without time zone,
+    cc_expiry_date timestamp without time zone,
+    cc_credit_limit double precision
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE dim.test
+    OWNER to sqladmin;
+GO
+
+--Syntax Error: Incorrect syntax near without.
+--Syntax Error: Incorrect syntax near OWNER.
+--CREATE TABLE dim.test
+--(
+--    customer_id bigint,
+--    cc_unique_account_id bigint,
+--    outstanding_balance double precision,
+--    issue_date timestamp without time zone,
+--    cc_expiry_date timestamp without time zone,
+--    cc_credit_limit double precision
+--)
+--WITH (
+--    OIDS = FALSE
+--)
+--TABLESPACE pg_default;
+--
+--ALTER TABLE dim.test
+--    OWNER to sqladmin;
+
+
+
+GO
